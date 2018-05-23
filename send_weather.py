67f6726f7816c1fa2a4c02b29dac4a378c3b639b
jmarcus3 <jmarcus3@uchicago.edu>
2018-05-23 18:57:38 -0500

    refactored by Alex, Alex and Joe

--- send_weather.py
+++ send_weather.py
@@ -8,56 +8,34 @@ from sys import exit
 from urllib.request import urlopen
 from twilio.rest import Client
 import json
-
-zip = input("What's your zip code? ")
-
-if len(zip) != 5:
-    print("That's not a zip code!")
-    exit(1)
-
-if not zip.isdigit():
-    print("That's not a zip code!")
-    exit(1)
-
-# Get current temperature from OpenWeatherMap.org
-
-url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=" + keys.weather_api
-raw_json = urlopen(url).read()
-data = json.loads(raw_json)
-temp = data['main']['temp']
-print("It is", str(round(temp)), "degrees outside.")
+import weather
 
 
 # Send as text message
 
+def main():
+  user_info_collector = weather.UserInfoCollector()
+  weather_provider = weather.WeatherProvider()
+  user_info_collector.promptForZipcode()
+  temp = weather_provider.getWeather(user_info_collector._zip)
+  user_info_collector.promptForPhoneNumber()
 
-print()
-print("(Press CTRL-C now if you do not want to send a text message)")
-phone = input("What's your phone number (10 digits)? ")
-
-if len(phone) != 10:
-    print("That's not a phone number!")
-    exit(1)
-
-if not phone.isdigit():
-    print("That's not a phone number!")
-    exit(1)
-
-phone_confirmation = input("Enter it again please (10 digits)? ")
-if phone != phone_confirmation:
-    print("Those phone numbers don't match.")
-    exit(1)
+  print()
+  print("(Press CTRL-C now if you do not want to send a text message)")
 
 
-client = Client(keys.account_sid, keys.auth_token)
+  client = Client(keys.account_sid, keys.auth_token)
 
-sms_body = "It is " + str(round(temp)) + " degrees outside."
-message = client.messages.create(
-                              body= sms_body,
-                              from_= keys.from_number,
-                              to='+1' + phone
-                          )
+  sms_body = "It is " + str(round(temp)) + " degrees outside."
+  message = client.messages.create(
+                                body= sms_body,
+                                from_= keys.from_number,
+                                to='+1' + user_info_collector._phone
+                            )
 
-# If the message was successfully sent, the message object
-# will have a "sid" property that you can access:
-print("Message Sent! Tracking ID is:", message.sid)
+  # If the message was successfully sent, the message object
+  # will have a "sid" property that you can access:
+  print("Message Sent! Tracking ID is:", message.sid)
+
+if __name__ == "__main__":
+  main()
--- weather.py
+++ weather.py
@@ -1,1 +1,63 @@
 # This is the weather module.
+# Prompts for a zip code
+# and displays the current temperature
+# and also sends it as a text message to your phone.
+
+# Python 3.x
+import keys
+from sys import exit
+from urllib.request import urlopen
+from twilio.rest import Client
+import json
+
+class UserInfoCollector:
+  def __init__(self):
+    pass
+
+  def promptForZipcode(self):
+    zip = input("What's your zip code? ")
+    if self.checkZip(zip):
+      self._zip = zip
+    else:
+        exit(1)
+
+  def checkZip(self, zip):
+    if len(zip) != 5 or not zip.isdigit():
+      print("That's not a zip code!")
+      return False
+    return True
+      
+
+  def promptForPhoneNumber(self):
+    phone = input("What's your phone number (10 digits)? ")
+    if not self.checkNumber(phone):
+      exit(1)
+    phone_confirmation = input("Enter it again please (10 digits)? ")
+    if phone == phone_confirmation:
+      self._phone = phone
+    else:
+      print("Those phone numbers don't match.")
+      exit(1)
+
+  def checkNumber(self, phone):
+    if len(phone) != 10 or not phone.isdigit():
+      print("That's not a phone number!")
+      return False
+    return True
+
+
+# phone_confirmation = input("Enter it again please (10 digits)? ")
+# if phone != phone_confirmation:
+#     print("Those phone numbers don't match.")
+    
+
+
+# Get current temperature from OpenWeatherMap.org
+class WeatherProvider:
+  def getWeather(self, zip):
+    url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=" + keys.weather_api
+    raw_json = urlopen(url).read()
+    data = json.loads(raw_json)
+    temp = data['main']['temp']
+    print("It is", str(round(temp)), "degrees outside.")
+    return temp
